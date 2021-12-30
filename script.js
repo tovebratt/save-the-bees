6e14b27d8052b7a6d96b421572369ecda6ba1d35
Bjunkz <b.eriksson91@gmail.com>
2021-12-30 09:38:55 +0100

    hide cookiebanner on button-press with JS

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("hej")
+//hide cookie-banner when pressing OK button
 
+const cookieBanner = document.querySelector(".cookie-banner")
+const cookieBtn = document.querySelector(".cookie-btn")
+
+cookieBtn.addEventListener("click", () => {
+    cookieBanner.style.display = 'none'
+})
